985c9eaae1383538ac2da5a353fb3f0d5138ccb6
AyeshaSangani <142738168+AyeshaSangani@users.noreply.github.com>
2024-10-26 20:53:01 +0500

    practice{24-09-24}

---
+++ Reg-Form.js
@@ -0,0 +1,10 @@
+function submitData(){
+    let username = document.getElementById("username").value
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+ 
+    console.log(username);
+    console.log(email);
+    console.log(password);
+    
+}
